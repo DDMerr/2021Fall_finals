1a35dec1931e580a25d76958db46eea29e43f88d
jiaa98 <89928563+jiaa98@users.noreply.github.com>
2021-12-12 01:12:44 -0600

    Update Nanjing_analysis.py

--- Nanjing_analysis.py
+++ Nanjing_analysis.py
@@ -1,29 +1,34 @@
+#Yunjia Zhang's part
+import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import warnings
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
+from collections import Counter
+from pyecharts import options as opts
+from pyecharts.charts import Map
+from pyecharts.faker import Faker
 
 
+#Read data
 data = pd.read_excel("nanjing.xlsx")
 df = data.iloc[:,0:9]
 df.columns = ['name','type','state','address','addresses','room','area','price','second']
-print(df)
-print(df.shape)
-
-print(df.isnull().sum())
+#print(df)
+#print(df.shape)#View data dimensions
+#print(df.isnull().sum())#Detect null value
 df.dropna(subset=['addresses','price','room','area','second'],inplace=True)
-print(df.isnull().sum())
-print(df.duplicated().sum())
-
-print(df.shape)
-
-
-
+#print(df.isnull().sum())
+#print(df.duplicated().sum())#Detection of duplicate values
+#print(df.shape)
 data1 = df.iloc[:,0:9]
-print(data1)
+#print(data1)
 
 
 
+#Convert text-based features to numeric
 
 warnings.filterwarnings("ignore")
 df["name"] = pd.factorize(df["name"])[0].astype(np.uint16)
@@ -38,137 +43,128 @@ df["second"] = pd.factorize(df["second"])[0].astype(np.uint16)
 print(df.head())
 
 
+#Standardization processing and normalization processing
 
 df.head(20)
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()#标准化
-data = pd.DataFrame(scaler.fit_transform(df[['name','type','state','address','addresses','room','area',"price",'second']]),columns=['name','type','state','address','addresses','room','area',"price",'second'])
+scaler = StandardScaler()  #standardization
+data = pd.DataFrame(scaler.fit_transform(df[['name', 'type', 'state', 'address', 'addresses', 'room', 'area', "price", 'second']]),
+        columns=['name', 'type', 'state', 'address', 'addresses', 'room', 'area', "price", 'second'])
 print(data.head())
+print(df.describe()) #Data description
+#Normalization()
 
 
-print(df.describe())
+#Scatter graph
 
-
-import seaborn as sns
-
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False
 score = data1['type']
-price=data1['price']
-
-plt.scatter(x=score,y=price, s=10, c='b')
+price = data1['price']
+plt.scatter(x=score, y=price, color='red',marker='o',alpha=0.5, s=10)
+plt.title("The relationship between property type and price")  # set title
 plt.show()
 
-
-sns.distplot(data1['price'], bins=20)
+#Price Histogram
+sns.set()
+sns.distplot(data1['price'], bins=20, kde=True, rug=True, vertical=True)
+plt.title("Price density range")
 plt.show()
+#Histogram()
 
+#Address and price relationship histogram
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False
-quyu_price=data1.groupby('address')['price'].mean()
-quyu_price.sort_values(ascending=True,inplace=True)
-quyu_price.plot.bar(figsize=(12,7),color=['orange','moccasin','wheat','papayawhip','tan'],alpha=0.6)#画布大小和颜色
+Regional_price = data1.groupby('address')['price'].mean()  # Find the mean
+Regional_price.sort_values(ascending=True, inplace=True)  # Find the sort
+Regional_price.plot.bar(figsize=(12, 7), color=['orange', 'moccasin', 'wheat', 'papayawhip', 'tan'],
+                        alpha=0.6)  # Canvas size and color
 plt.ylabel('price')
+plt.title("Address and price relationship")
 plt.show()
 
+#Histogram_1()
 
+#Type and price relationship histogram
 
-quyu_price=data1.groupby('type')['price'].mean()
-quyu_price.sort_values(ascending=True,inplace=True)
-quyu_price.plot.bar(figsize=(12,8),color=['moccasin','orange','wheat','tan','papayawhip'],alpha=0.6)
+Regional_price = data1.groupby('type')['price'].mean()
+Regional_price.sort_values(ascending=True, inplace=True)
+Regional_price.plot.bar(figsize=(12, 8), color=['mistyrose','lightpink','pink', 'palevioletred', 'hotpink', 'violet', 'mediumorchid'], ec='k', lw=1, alpha=0.6)
 plt.ylabel('price')
+plt.title("Type and price relationship")
 plt.show()
 
+#Histogram_2()
 
+#Area and price relationship box plot
 
 hdate = data1['room']
-price=data1['price']
-f,ax = plt.subplots(figsize=(10,7))
-fig = sns.boxplot(x=hdate,y=price)
-fig.axis(ymin=10000,ymax=100500)
+price = data1['price']
+f,ax = plt.subplots(figsize=(10, 7))
+fig = sns.boxplot(x=hdate, y=price)
+fig.axis(ymin=10000, ymax=100500)
+plt.title("Area and price relationship")
 plt.show()
 
+#box_plot()
 
-fangyuan=data1.address.value_counts().sort_values()
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.rcParams['axes.unicode_minus']=False
-plt.figure(figsize=(15,13),dpi=80)
-explode={}
-for i in fangyuan.index:
-    if i in ['江宁','浦口','玄武']:
-        explode[i]=0
+#Pie chart of distribution of new houses in Nanjing
+
+Housing_resources = data1.address.value_counts().sort_values()
+
+plt.figure(figsize=(15, 13), dpi=80)
+explode = {}
+for i in Housing_resources.index:
+    if i in ['JiangNing', 'PuKou']:
+        explode[i] = 0
     else:
-        explode[i]=0
+        explode[i] = 1
 
-plt.pie(fangyuan,labels=fangyuan.index,explode=explode.values(),autopct='%0.2f%%',colors=sns.color_palette('hls', n_colors=16))
-plt.title('Nanjing Housing Distribution',fontsize=20)
+plt.pie(Housing_resources, labels=Housing_resources.index, explode=explode.values(), autopct='%0.2f%%',
+            colors=sns.color_palette('hls', n_colors=16))
+plt.title('Nanjing Housing Distribution', fontsize=20)
 plt.axis('equal')
 plt.legend(loc='upper left')
 plt.show()
 
+#pie_chart()
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-sns.set(font='SimHei')  # 解决Seaborn中文显示问题
+#Correlation thermodynamic diagram
+
 transformed_data = pd.get_dummies(data)
 colormap = plt.cm.RdBu
-plt.figure(figsize=(14,12))
+plt.figure(figsize=(14, 12))
+#Solve the problem that the negative sign '-' is displayed as a square in the saved image
 
 plt.title('Pearson Correlation of Features', y=1.05, size=15)
-sns.heatmap(transformed_data.astype(float).corr(),linewidths=0.1,vmax=1.0,
-            square=True, cmap=colormap, linecolor='white', annot=True)
+sns.heatmap(transformed_data.astype(float).corr(), linewidths=0.1, vmax=1.0, square=True, cmap=colormap, linecolor='white', annot=True)
 plt.show()
 
+#Thermodynamic_diagram()
 
+#Statistics address information
 
-
-
-
-
-from collections import Counter
 lista = data1['address']
 c = Counter(lista)
-print(c)
+#print(c)
 
+#nanjing map
 
-
-from pyecharts import options as opts
-from pyecharts.charts import Map
-from pyecharts.faker import Faker
+# Basic data
 
 quxian = ['江宁区', '浦口区',
           '玄武区', '鼓楼区', '雨花台区',
           '六合区', '栖霞区', '溧水区', '高淳区',
-          '建邺区','句容区','秦淮区']
+          '建邺区', '句容区', '秦淮区']
 
 values3 = [106, 97, 10, 18, 24, 30, 42, 38, 29, 24, 16, 15]
 
-english_names = {"江宁区":"JIANGNING",
-                '浦口区':"PUKOU",
-                '玄武区':"XUANWU",
-                '鼓楼区':"GULOU",
-                '雨花台区':"YUHUATAI",
-                '六合区':"LIUHE",
-                '栖霞区':"QIXIA",
-                '溧水区':"LISHUI",
-                '高淳区':"GAOCHUN",
-                '建邺区':"JIANYE",
-                '句容区':"JURONG",
-                '秦淮区':"QINHUAI"}
-
-
 c = (
     Map()
-        .add("南京", [list(z) for z in zip(quxian, values3)], "南京", name_map=english_names)
+        .add("南京", [list(z) for z in zip(quxian, values3)], "南京")
         .set_global_opts(
         title_opts=opts.TitleOpts(title="Nanjing map"), visualmap_opts=opts.VisualMapOpts()
     )
         .render()
 )
 
-import os
 os.system("open render.html")
+
+
 
